fabde0cdf21dd979180052813733a9a95fc3eb2a
Kalle Jillheden <kalle.jillheden@iver.se>
2020-05-07 08:02:51 +0200

    Changed to variable helm names

--- README.md
+++ README.md
@@ -9,11 +9,26 @@ This gist does the same for helm, but also for helm2 and helm3, in case you were
 For example, if you were to download and install
 
 - `https://get.helm.sh/helm-v2.16.7-linux-amd64.tar.gz` into `/usr/local/bin/helm2`
-- `https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz` into `/usr/local/bin/helm`
+- `https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz` into `/usr/local/bin/helm3`
 
 Then you would get correct completion for both cases.
 
 ## Installation
+
+### Helm
+
+By default the completions assume you have installed Helm v2 as `helm2` and
+Helm v3 as `helm3`.
+
+To change this default, set the variables `HELM_2_BIN` and/or `HELM_3_BIN`
+respectively, before the plugin gets loaded.
+
+If you have Helm v2 installed as just `helm`, and Helm v3 as `helm3` you only
+need to set the `HELM_2_BIN` environment variable like so:
+
+```zsh
+export HELM_2_BIN=helm
+```
 
 ### Oh My Zsh
 
--- helm-2n3.plugin.zsh
+++ helm-2n3.plugin.zsh
@@ -1,22 +1,10 @@
 
-if (( $+commands[helm] )); then
-    __HELM_COMPLETION_FILE="${ZSH_CACHE_DIR}/helm_completion"
-
-    if [[ ! -f $__HELM_COMPLETION_FILE ]]; then
-        helm completion zsh >! $__HELM_COMPLETION_FILE
-    fi
-
-    [[ -f $__HELM_COMPLETION_FILE ]] && source $__HELM_COMPLETION_FILE
-
-    unset __HELM_COMPLETION_FILE
-fi
-
-if (( $+commands[helm2] )); then
+if (( $+commands[${HELM_2_BIN:=helm2}] )); then
     __HELM_COMPLETION_FILE="${ZSH_CACHE_DIR}/helm2_completion"
 
     if [[ ! -f $__HELM_COMPLETION_FILE ]]; then
-        helm2 completion zsh >! $__HELM_COMPLETION_FILE
-        sed -i 's/^#compdef helm$/#compdef helm2/' $__HELM_COMPLETION_FILE
+        $HELM_2_BIN completion zsh >! $__HELM_COMPLETION_FILE
+        sed -i "s/^#compdef helm\$/#compdef $HELM_2_BIN/" $__HELM_COMPLETION_FILE
     fi
 
     [[ -f $__HELM_COMPLETION_FILE ]] && source $__HELM_COMPLETION_FILE
@@ -24,12 +12,12 @@ if (( $+commands[helm2] )); then
     unset __HELM_COMPLETION_FILE
 fi
 
-if (( $+commands[helm3] )); then
+if (( $+commands[${HELM_3_BIN:=helm3}] )); then
     __HELM_COMPLETION_FILE="${ZSH_CACHE_DIR}/helm3_completion"
 
     if [[ ! -f $__HELM_COMPLETION_FILE ]]; then
-        helm3 completion zsh >! $__HELM_COMPLETION_FILE
-        sed -i 's/^#compdef helm$/#compdef helm3/' $__HELM_COMPLETION_FILE
+        $HELM_3_BIN completion zsh >! $__HELM_COMPLETION_FILE
+        sed -i "s/^#compdef helm$/#compdef $HELM_3_BIN/" $__HELM_COMPLETION_FILE
     fi
 
     [[ -f $__HELM_COMPLETION_FILE ]] && source $__HELM_COMPLETION_FILE
